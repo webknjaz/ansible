45bfbb15a70a63c5e7b77316f845577ed9ee3a91
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2018-03-29 16:13:43 +0200

    Fix foreman's runme in tests

--- test/integration/targets/foreman_inventory/runme.sh
+++ test/integration/targets/foreman_inventory/runme.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-set -euo pipefail
-
 [[ -n "$DEBUG" ]] && set -x
 
-ansible-playbook test_foreman_inventory -v "$@"
+set -euo pipefail
+
+ansible-playbook test_foreman_inventory.yml -v "$@"
