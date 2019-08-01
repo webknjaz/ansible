020f52266c453e60e220e1f0892572b68b0a138c
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2019-08-01 17:36:58 +0200

    🐛 Fix pytest ability to patch stdout

--- lib/ansible/module_utils/_stdout_utils.py
+++ lib/ansible/module_utils/_stdout_utils.py
@@ -8,7 +8,11 @@ from ansible.module_utils.six import PY3
 __all__ = ('write_bytes_to_stdout', )
 
 
-write_bytes_to_stdout = (  # pylint: disable=invalid-name
-    sys.stdout.buffer.write if PY3
-    else sys.stdout.write
-)
+def write_bytes_to_stdout(*args, **kwargs):
+    """Write bytes to stdout and flush immediately."""
+    _write_stdout_stream = (
+        sys.stdout.buffer if PY3
+        else sys.stdout
+    ).write
+    _write_stdout_stream(*args, **kwargs)
+    sys.stdout.flush()
