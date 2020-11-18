b52b85e27b557baaf99fa0b1a1796ea6d2998e8e
s-hertel <shertel@redhat.com>
2020-11-18 17:19:01 -0500

    Handle null collections and roles in requirements.yml

--- lib/ansible/cli/galaxy.py
+++ lib/ansible/cli/galaxy.py
@@ -639,7 +639,7 @@ class GalaxyCLI(CLI):
                     else {'name': collection_req},
                     artifacts_manager,
                 )
-                for collection_req in file_requirements.get('collections', [])
+                for collection_req in file_requirements.get('collections') or []
             ]
 
         return requirements
